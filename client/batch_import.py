8fa07c6a4410fd7237775a2b43a70327a5e5efef
Kenneth Chan <kenneth@prediction.io>
2014-05-02 17:33:52 -0700

    Convert tab to spaces

--- client/batch_import.py
+++ client/batch_import.py
@@ -7,64 +7,64 @@ from app_config import APP_KEY, API_URL, THREADS, REQUEST_QSIZE
 
 def batch_import_task(app_data, client, all_info=False):
 
-	print "[Info] Importing users to PredictionIO..."
-	count = 0
-	for k, v in app_data.get_users().iteritems():
-		count += 1
-		if all_info:
-			print "[Info] Importing %s..." % v
-		else:
-			if (count % 32 == 0):
-				sys.stdout.write('\r[Info] %s' % count)
-				sys.stdout.flush()
+  print "[Info] Importing users to PredictionIO..."
+  count = 0
+  for k, v in app_data.get_users().iteritems():
+    count += 1
+    if all_info:
+      print "[Info] Importing %s..." % v
+    else:
+      if (count % 32 == 0):
+        sys.stdout.write('\r[Info] %s' % count)
+        sys.stdout.flush()
 
-		client.acreate_user(v.uid)
+    client.acreate_user(v.uid)
 
-	sys.stdout.write('\r[Info] %s users were imported.\n' % count)
-	sys.stdout.flush()
+  sys.stdout.write('\r[Info] %s users were imported.\n' % count)
+  sys.stdout.flush()
 
-	print "[Info] Importing items to PredictionIO..."
-	count = 0
-	for k, v in app_data.get_items().iteritems():
-		count += 1
-		if all_info:
-			print "[Info] Importing %s..." % v
-		else:
-			if (count % 32 == 0):
-				sys.stdout.write('\r[Info] %s' % count)
-				sys.stdout.flush()
+  print "[Info] Importing items to PredictionIO..."
+  count = 0
+  for k, v in app_data.get_items().iteritems():
+    count += 1
+    if all_info:
+      print "[Info] Importing %s..." % v
+    else:
+      if (count % 32 == 0):
+        sys.stdout.write('\r[Info] %s' % count)
+        sys.stdout.flush()
 
-		itypes = ("movie",) + v.genres
-		client.acreate_item(v.iid, itypes,
-			{ "pio_startT" : v.release_date.isoformat(),
-			  "name" : v.name,
-				"year" : v.year } )
+    itypes = ("movie",) + v.genres
+    client.acreate_item(v.iid, itypes,
+      { "pio_startT" : v.release_date.isoformat(),
+        "name" : v.name,
+        "year" : v.year } )
 
-	sys.stdout.write('\r[Info] %s items were imported.\n' % count)
-	sys.stdout.flush()
+  sys.stdout.write('\r[Info] %s items were imported.\n' % count)
+  sys.stdout.flush()
 
-	print "[Info] Importing rate actions to PredictionIO..."
-	count = 0
-	for v in app_data.get_rate_actions():
-		count += 1
-		if all_info:
-			print "[Info] Importing %s..." % v
-		else:
-			if (count % 32 == 0):
-				sys.stdout.write('\r[Info] %s' % count)
-				sys.stdout.flush()
+  print "[Info] Importing rate actions to PredictionIO..."
+  count = 0
+  for v in app_data.get_rate_actions():
+    count += 1
+    if all_info:
+      print "[Info] Importing %s..." % v
+    else:
+      if (count % 32 == 0):
+        sys.stdout.write('\r[Info] %s' % count)
+        sys.stdout.flush()
 
-		client.identify(v.uid)
-		client.arecord_action_on_item("rate", v.iid,
-			{ "pio_rate": v.rating, "pio_t": v.t })
+    client.identify(v.uid)
+    client.arecord_action_on_item("rate", v.iid,
+      { "pio_rate": v.rating, "pio_t": v.t })
 
-	sys.stdout.write('\r[Info] %s rate actions were imported.\n' % count)
-	sys.stdout.flush()
+  sys.stdout.write('\r[Info] %s rate actions were imported.\n' % count)
+  sys.stdout.flush()
 
 
 if __name__ == '__main__':
 
-	app_data = AppData()
-	client = predictionio.Client(APP_KEY, THREADS, API_URL, qsize=REQUEST_QSIZE)
-	batch_import_task(app_data, client)
-	client.close()
+  app_data = AppData()
+  client = predictionio.Client(APP_KEY, THREADS, API_URL, qsize=REQUEST_QSIZE)
+  batch_import_task(app_data, client)
+  client.close()
--- client/movie_rec_app.py
+++ client/movie_rec_app.py
@@ -11,214 +11,214 @@ SIM_ENGINE_NAME = 'movie-sim'
 
 class App:
 
-	def __init__(self):
-		self._app_data = AppData()
-		self._client = predictionio.Client(APP_KEY, 1, API_URL)
+  def __init__(self):
+    self._app_data = AppData()
+    self._client = predictionio.Client(APP_KEY, 1, API_URL)
 
-	def run(self):
-		state = "[Main Menu]"
+  def run(self):
+    state = "[Main Menu]"
 
-		prompt = "\n"\
-			"%s\n"\
-			"%s\n"\
-			"Please input selection:\n"\
-			" 0: Quit application.\n"\
-			" 1: Get personalized recommendation.\n"\
-			" 2: Display user data.\n"\
-			" 3: Display movie data.\n"\
-			" 4: Recommend with multiple movies.\n" % (state, '-'*len(state))
+    prompt = "\n"\
+      "%s\n"\
+      "%s\n"\
+      "Please input selection:\n"\
+      " 0: Quit application.\n"\
+      " 1: Get personalized recommendation.\n"\
+      " 2: Display user data.\n"\
+      " 3: Display movie data.\n"\
+      " 4: Recommend with multiple movies.\n" % (state, '-'*len(state))
 
-		while True:
-			print prompt
-			choice = raw_input().lower()
-			if choice == '0':
-				print "\nGood Bye!\n"
-				break
-			elif choice == '1':
-				self.recommend_task(state)
-			elif choice == '2':
-				self.display_user_task(state)
-			elif choice == '3':
-				self.get_similar_movies_task(state)
-			elif choice == '4':
-				self.recommend_with_multiple_movies_task(state)
-			else:
-				print '[Error] \'%s\' is not a valid selection.' % choice
+    while True:
+      print prompt
+      choice = raw_input().lower()
+      if choice == '0':
+        print "\nGood Bye!\n"
+        break
+      elif choice == '1':
+        self.recommend_task(state)
+      elif choice == '2':
+        self.display_user_task(state)
+      elif choice == '3':
+        self.get_similar_movies_task(state)
+      elif choice == '4':
+        self.recommend_with_multiple_movies_task(state)
+      else:
+        print '[Error] \'%s\' is not a valid selection.' % choice
 
-		self._client.close()
+    self._client.close()
 
-	def recommend_task(self, prev_state):
-		state = prev_state + " / [Get Recommendations]"
-		prompt = "\n"\
-			"%s\n"\
-			"%s\n"\
-			"Please enter user id:" % (state, '-'*len(state))
+  def recommend_task(self, prev_state):
+    state = prev_state + " / [Get Recommendations]"
+    prompt = "\n"\
+      "%s\n"\
+      "%s\n"\
+      "Please enter user id:" % (state, '-'*len(state))
 
-		while True:
-			print prompt
-			choice = raw_input().lower()
-			u = self._app_data.get_user(choice)
-			if u:
-				n = 10
-				print "[Info] Getting top %s item recommendations for user %s..." % (n, u.uid)
-				try:
-					self._client.identify(u.uid)
-					rec = self._client.get_itemrec_topn(ENGINE_NAME, n)
-					u.rec = rec['pio_iids']
-					self.display_items(u.rec)
-				except predictionio.ItemRecNotFoundError:
-					print "[Info] Recommendation not found"
+    while True:
+      print prompt
+      choice = raw_input().lower()
+      u = self._app_data.get_user(choice)
+      if u:
+        n = 10
+        print "[Info] Getting top %s item recommendations for user %s..." % (n, u.uid)
+        try:
+          self._client.identify(u.uid)
+          rec = self._client.get_itemrec_topn(ENGINE_NAME, n)
+          u.rec = rec['pio_iids']
+          self.display_items(u.rec)
+        except predictionio.ItemRecNotFoundError:
+          print "[Info] Recommendation not found"
 
-				print "[Info] Go back to previous menu..."
-				break
-			else:
-				print "[Error] invalid user id %s. Go back to previous menu..." % choice
-				break
+        print "[Info] Go back to previous menu..."
+        break
+      else:
+        print "[Error] invalid user id %s. Go back to previous menu..." % choice
+        break
 
-	def get_similar_movies_task(self, prev_state):
-		state = prev_state + " / [Get Similar Movies]"
-		prompt = "\n"\
-			"%s\n"\
-			"%s\n"\
-			"Please enter movie id (eg. 1):" % (state, '-'*len(state))
+  def get_similar_movies_task(self, prev_state):
+    state = prev_state + " / [Get Similar Movies]"
+    prompt = "\n"\
+      "%s\n"\
+      "%s\n"\
+      "Please enter movie id (eg. 1):" % (state, '-'*len(state))
 
-		while True:
-			print prompt
-			choice = raw_input().lower()
-			i = self._app_data.get_item(choice)
+    while True:
+      print prompt
+      choice = raw_input().lower()
+      i = self._app_data.get_item(choice)
 
-			if i:
-				n = 10
-				self.display_items((i.iid,), all_info=False)
-				print "\n[Info] People who liked this may also liked..."
-				try:
-					rec = self._client.get_itemsim_topn(SIM_ENGINE_NAME, i.iid, n,
-						{ 'pio_itypes' : i.genres })
-					self.display_items(rec['pio_iids'], all_info=False)
-				except predictionio.ItemSimNotFoundError:
-					print "[Info] Similar movies not found"
+      if i:
+        n = 10
+        self.display_items((i.iid,), all_info=False)
+        print "\n[Info] People who liked this may also liked..."
+        try:
+          rec = self._client.get_itemsim_topn(SIM_ENGINE_NAME, i.iid, n,
+            { 'pio_itypes' : i.genres })
+          self.display_items(rec['pio_iids'], all_info=False)
+        except predictionio.ItemSimNotFoundError:
+          print "[Info] Similar movies not found"
 
-				print "[Info] Go back to previous menu..."
-				break
-			else:
-				print "[Error] invalid item id %s. Go back to previous menu..." % choice
-				break
+        print "[Info] Go back to previous menu..."
+        break
+      else:
+        print "[Error] invalid item id %s. Go back to previous menu..." % choice
+        break
 
-	def recommend_with_multiple_movies_task(self, prev_state):
-		state = prev_state + " / [Recommend with Multiple Movies]"
-		prompt = "\n"\
-			"%s\n"\
-			"%s\n"\
-			"Please enter comma separated movie ids (eg. 1,2,3):" % (state, '-'*len(state))
+  def recommend_with_multiple_movies_task(self, prev_state):
+    state = prev_state + " / [Recommend with Multiple Movies]"
+    prompt = "\n"\
+      "%s\n"\
+      "%s\n"\
+      "Please enter comma separated movie ids (eg. 1,2,3):" % (state, '-'*len(state))
 
-		while True:
-			print prompt
-			choice = raw_input().lower()
-			viewed_iids = choice.split(",")
-			viewed_items = map(lambda x : self._app_data.get_item(x), viewed_iids)
+    while True:
+      print prompt
+      choice = raw_input().lower()
+      viewed_iids = choice.split(",")
+      viewed_items = map(lambda x : self._app_data.get_item(x), viewed_iids)
 
-			viewed_genres = Set()
-			for i in viewed_items:
-				if i:
-					for g in i.genres:
-						viewed_genres.add(g)
+      viewed_genres = Set()
+      for i in viewed_items:
+        if i:
+          for g in i.genres:
+            viewed_genres.add(g)
 
-			if None not in viewed_items:
-				n = 10
-				self.display_items(viewed_iids, all_info=False)
-				print "\n[Info] Top %s similar movies..." % n
-				try:
-					rec = self._client.get_itemsim_topn(SIM_ENGINE_NAME, choice, n,
-						{ 'pio_itypes' : list(viewed_genres) })
-					self.display_items(rec['pio_iids'], all_info=False)
-				except predictionio.ItemSimNotFoundError:
-					print "[Info] Similar movies not found"
+      if None not in viewed_items:
+        n = 10
+        self.display_items(viewed_iids, all_info=False)
+        print "\n[Info] Top %s similar movies..." % n
+        try:
+          rec = self._client.get_itemsim_topn(SIM_ENGINE_NAME, choice, n,
+            { 'pio_itypes' : list(viewed_genres) })
+          self.display_items(rec['pio_iids'], all_info=False)
+        except predictionio.ItemSimNotFoundError:
+          print "[Info] Similar movies not found"
 
-				print "[Info] Go back to previous menu..."
-				break
-			else:
-				print "[Error] invalid item id %s. Go back to previous menu..." % choice
-				break
+        print "[Info] Go back to previous menu..."
+        break
+      else:
+        print "[Error] invalid item id %s. Go back to previous menu..." % choice
+        break
 
-	def display_user_task(self, prev_state):
-		state = prev_state + " / [Display User]"
-		prompt = "\n"\
-			"%s\n"\
-			"%s\n"\
-			"Please enter user id:" % (state, '-'*len(state))
+  def display_user_task(self, prev_state):
+    state = prev_state + " / [Display User]"
+    prompt = "\n"\
+      "%s\n"\
+      "%s\n"\
+      "Please enter user id:" % (state, '-'*len(state))
 
-		while True:
-			print prompt
-			choice = raw_input().lower()
-			u = self._app_data.get_user(choice)
-			if u:
-				print "[Info] User %s:" % u.uid
-				n = 10
-				topn_rate_actions = self._app_data.get_top_rate_actions(u.uid, n)
-				print "\n[Info] Top %s movies rated by this user:" % n
-				self.display_rate_actions(topn_rate_actions)
+    while True:
+      print prompt
+      choice = raw_input().lower()
+      u = self._app_data.get_user(choice)
+      if u:
+        print "[Info] User %s:" % u.uid
+        n = 10
+        topn_rate_actions = self._app_data.get_top_rate_actions(u.uid, n)
+        print "\n[Info] Top %s movies rated by this user:" % n
+        self.display_rate_actions(topn_rate_actions)
 
-				print "\n[Info] Getting New Recommendation..."
-				n = 10
-				try:
-					self._client.identify(u.uid)
-					rec = self._client.get_itemrec_topn(ENGINE_NAME, n)
-					u.rec = rec['pio_iids']
-				except predictionio.ItemRecNotFoundError:
-					print "[Info] Recommendation not found"
+        print "\n[Info] Getting New Recommendation..."
+        n = 10
+        try:
+          self._client.identify(u.uid)
+          rec = self._client.get_itemrec_topn(ENGINE_NAME, n)
+          u.rec = rec['pio_iids']
+        except predictionio.ItemRecNotFoundError:
+          print "[Info] Recommendation not found"
 
-				print "\n[Info] Movies recommended to this user:"
-				self.display_items(u.rec)
+        print "\n[Info] Movies recommended to this user:"
+        self.display_items(u.rec)
 
-				self.wait_for_ack()
-				print "\n[Info] Go back to previous menu..."
-				break
-			else:
-				print "[Error] invalid user id %s. Go back to previous menu..." % choice
-				break
+        self.wait_for_ack()
+        print "\n[Info] Go back to previous menu..."
+        break
+      else:
+        print "[Error] invalid user id %s. Go back to previous menu..." % choice
+        break
 
-	def display_items(self, iids, all_info=False):
-		"""print item info for each iid in the list
-		"""
-		if iids:
-			for iid in iids:
-				item = self._app_data.get_item(iid)
-				if item:
-					if all_info:
-						print "[Info] %s" % item
-					else:
-						print "[Info] (%s) %s %s %s" % (item.iid, item.name,
-							item.release_date.strftime("%d-%b-%Y"), item.genres)
-				else:
-					print "[Error] Invalid item id %s" % iid
-		else:
-			print "[Info] Empty."
+  def display_items(self, iids, all_info=False):
+    """print item info for each iid in the list
+    """
+    if iids:
+      for iid in iids:
+        item = self._app_data.get_item(iid)
+        if item:
+          if all_info:
+            print "[Info] %s" % item
+          else:
+            print "[Info] (%s) %s %s %s" % (item.iid, item.name,
+              item.release_date.strftime("%d-%b-%Y"), item.genres)
+        else:
+          print "[Error] Invalid item id %s" % iid
+    else:
+      print "[Info] Empty."
 
-	def display_rate_actions(self, actions):
-		"""print iid and rating
-		"""
-		if actions:
-			for a in actions:
-				item = self._app_data.get_item(a.iid)
-				if item:
-					print "[Info] (%s) %s %s %s, rating = %s" % (item.iid, item.name,
-						item.release_date.strftime("%d-%b-%Y"), item.genres, a.rating)
-				else:
-					print "[Error] Invalid item id %s" % a.iid
-		else:
-			print "[Info] Empty."
+  def display_rate_actions(self, actions):
+    """print iid and rating
+    """
+    if actions:
+      for a in actions:
+        item = self._app_data.get_item(a.iid)
+        if item:
+          print "[Info] (%s) %s %s %s, rating = %s" % (item.iid, item.name,
+            item.release_date.strftime("%d-%b-%Y"), item.genres, a.rating)
+        else:
+          print "[Error] Invalid item id %s" % a.iid
+    else:
+      print "[Info] Empty."
 
-	def wait_for_ack(self):
+  def wait_for_ack(self):
 
-		prompt = "\nPress enter to continue..."
-		print prompt
-		choice = raw_input().lower()
+    prompt = "\nPress enter to continue..."
+    print prompt
+    choice = raw_input().lower()
 
 
 if __name__ == '__main__':
 
-	print "\nWelcome To PredictionIO Python-SDK Demo App!"
-	print "============================================\n"
+  print "\nWelcome To PredictionIO Python-SDK Demo App!"
+  print "============================================\n"
 
-	my_app = App()
-	my_app.run()
+  my_app = App()
+  my_app.run()
